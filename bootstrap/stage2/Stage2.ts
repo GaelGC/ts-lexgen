ce53d6578f01cfa527b5ff25bfc7e952714d5637
Gael GC <83537366+GaelGC@users.noreply.github.com>
2021-05-10 01:21:43 +0100

    [BOOTSTRAP] Start stage 2

---
+++ bootstrap/stage2/Stage2.ts
@@ -0,0 +1,111 @@
+import fs = require('fs');
+import { EOF } from '../../src/Automaton';
+import { Matcher } from "../../src/Matcher";
+import { getBytes, getString, LitteralNode, RegexNode } from "../../src/RegexNodes";
+import { Lexer, ruleNames } from "./Stage2Lexer";
+import { LexerGenerator } from "../../src/LexerGenerator";
+import { handleChar, handleSpecialChar, pop, unEscape } from "../helpers/CharacterHandlers";
+
+enum State {
+    Initial,
+    NameAcquired,
+    StateDeclaration,
+    DefinitionOngoing
+}
+
+class Stage2 extends Lexer {
+    private sent: boolean = false;
+    getBytes(): number[] {
+        if (this.sent) {
+            return [];
+        }
+        this.sent = true;
+        return getBytes(fs.readFileSync("./bootstrap/stage3/stage3.l").toString());
+    }
+}
+
+const generator = new LexerGenerator();
+const stage2 = new Stage2();
+var outputLexer = new Matcher();
+
+var curState: State = State.Initial;
+var curName = "";
+var nodes: RegexNode[] = [new LitteralNode("")];
+var opStack: string[] = [];
+var stateName = "INITIAL";
+var eof = false;
+
+while (!eof) {
+    var res = stage2.lex();
+    var lexem: [string, string] = ["NEWLINE", "\n"];
+    if (res === undefined) {
+        console.log("Error");
+        break;
+    } else if (res instanceof EOF) {
+        eof = true;
+        break;
+    }
+    lexem = [stage2.getRuleNames()[res[0]], getString(res[1])];
+    console.log(`Rule ${lexem[0]} reached with string ${lexem[1]}`);
+    switch (curState) {
+        case State.Initial: {
+            console.log(lexem[1]);
+            if (lexem[0] !== "NEWLINE" && lexem[0] !== "ID" && lexem[0] !== "STATE") {
+                throw Error(`Unexpected ${lexem[0]} at beginning of line.`);
+            }
+            if (lexem[0] === "ID") {
+                curName = lexem[1];
+                curState = State.NameAcquired;
+            }
+            if (lexem[0] === "STATE") {
+                curState = State.StateDeclaration;
+            }
+            break;
+        }
+        case State.NameAcquired: {
+            if (lexem[0] !== "SEPARATOR") {
+                throw Error("Expected =>");
+            }
+            curState = State.DefinitionOngoing;
+            break;
+        }
+        case State.StateDeclaration: {
+            if (lexem[0] === "ID") {
+                generator.addStateMatcher(stateName, outputLexer);
+                stateName = lexem[1];
+                outputLexer = new Matcher();
+            }
+            if (lexem[0] === "NEWLINE") {
+                curState = State.Initial;
+            }
+            break;
+        }
+        case State.DefinitionOngoing: {
+            var c = lexem[1];
+            if (lexem[0] === "ESCAPE") {
+                c = unEscape(c);
+            }
+            if (lexem[0] === "SPECIAL") {
+                handleSpecialChar(c, nodes, opStack);
+            } else if (lexem[0] === "NEWLINE") {
+                while (opStack.length !== 0) {
+                    pop(nodes, opStack);
+                }
+                console.log(`Registering rule ${curName} with content ${nodes[0].toString()}`);
+                outputLexer.registerRule(curName, nodes[0]);
+                nodes = [new LitteralNode("")];
+                curState = State.Initial;
+            } else {
+                handleChar(c, nodes, opStack);
+            }
+            break;
+        }
+    }
+}
+
+generator.addStateMatcher(stateName, outputLexer);
+const code = generator.compile("../../src/");
+
+fs.writeFileSync("./bootstrap/stage3/Stage3Lexer.ts", code);
+
+
